6e481d891e1e31f55ed4f041f985e321b317ba6e
Rahka69 <hafidzrakha17@gmail.com>
2025-05-20 20:18:34 +0700

    percobaan kedua

---
+++ day-1/string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hafidz")
+	fmt.Println("Hafidz Muhammad")
+	fmt.Println("Hafidz muhammad Rakha")
+	fmt.Println("Hafidz Muhammad Rakha Shidqi")
+}
