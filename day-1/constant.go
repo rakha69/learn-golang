e5bd936174162e98df8295d5239784dd2695e7b1
Rahka69 <hafidzrakha17@gmail.com>
2025-05-21 23:52:03 +0700

    percobaan ketiga

---
+++ day-1/constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstname string = "Hafidz"
+		fullname         = "Hafidz Muhammad Rakha Shidqi"
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(fullname)
+
+	//eror
+	//fullname = "joko"
+	//fullname = "riki"
+}
---
+++ day-1/variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Hafidz muhammad Rakha Shidqi"
+	fmt.Println(name)
+
+	name = "Muhammad Rakha"
+	fmt.Println(name)
+
+	var (
+		firstname = "Hafidz"
+		fullname  = "Hafidz Muhammad Rakha Shidqi"
+	)
+	fmt.Println(firstname)
+	fmt.Println(fullname)
+}
