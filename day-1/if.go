f1385d4f63a5ab04a9b3e81a37bd9bc49ece7b39
Rahka69 <hafidzrakha17@gmail.com>
2025-06-26 22:43:14 +0700

    penambahan

--- day-1/if.go
+++ day-1/if.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	name := "Rakha	"
+	name := "Rakha"
 
 	if name == "Rakha" {
 		fmt.Println("Hallo Rakha")
+	} else if name == "Hafidz" {
+		fmt.Println("Hallo Hafidz")
+	} else {
+		fmt.Println("Hi, Boleh kenalan?")
 	}
 
+	length := len(name)
+	if length > 5 {
+		fmt.Println("Nama Terlalu Panjang")
+	} else {
+		fmt.Println("Nama sudah benar")
+	}
 }
